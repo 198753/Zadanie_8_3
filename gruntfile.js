083b782d5fa156c97baddd0052528d8becf3346d
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-09-23 10:27:12 +0200

    próbuję dodać sass

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+
+
+  grunt.initConfig({
+  	sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
+        }
+      }
+    },
+    jshint: {
+
+      all: ['js/*.js']
+
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+
+
+  grunt.registerTask('default', ['jshint', 'sass']);
+
+
+
+};
